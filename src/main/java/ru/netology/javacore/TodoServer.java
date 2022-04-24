0c83fe989b2512cdb9c7c04be32224aa27901baa
Denis Alimov <de.alimov@mail.ru>
2022-04-24 22:04:15 +0300

    Не знаю, в чем проблема в сервере

--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -23,30 +23,31 @@ public class TodoServer {
 
     public void start() throws IOException {
         System.out.println("Starting server at " + port + "...");
-        ServerSocket serverSocket = new ServerSocket(port);
+        try (ServerSocket serverSocket = new ServerSocket(port)) { // Сервер сокет создаю один раз
 
-        Gson gson = new Gson();
+            Gson gson = new Gson();
 
-        while (true) {
-            try (Socket clientSocket = serverSocket.accept();
-                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
 
-                String json = in.readLine();
-                Map map = gson.fromJson(json, Map.class);
+                    String json = in.readLine(); //Один строковый запрос из клиента.
+                    Map map = gson.fromJson(json, Map.class);
 
-                switch (map.get("type").toString()) {
-                    case ("ADD"):
-                        todos.addTask(map.get("task").toString());
-                        break;
-                    case ("REMOVE"):
-                        todos.removeTask(map.get("task").toString());
-                        break;
+                    switch (map.get("type").toString()) {
+                        case ("ADD"):
+                            todos.addTask(map.get("task").toString());
+                            break;
+                        case ("REMOVE"):
+                            todos.removeTask(map.get("task").toString());
+                            break;
+                    }
+                    out.println(todos.getAllTasks()); //Ответ в одну строку (стрингбилдер собирает)
                 }
-                out.println(todos.getAllTasks());
-            } catch (IIOException e) {
-                e.printStackTrace();
             }
+        } catch (IIOException e) { // Try with res автоматически закрывает подключение и потоки
+            e.printStackTrace();
         }
     }
 }
